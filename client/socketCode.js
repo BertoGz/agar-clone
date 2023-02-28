d5475d16b075a22979408391b43f0508da5241fb
bertogz <bertogzlx@gmail.com>
2023-02-28 14:07:33 -0800

    add event listeners for handling orb collisions, player pos, leaderboard

--- client/socketCode.js
+++ client/socketCode.js
@@ -1,5 +1,4 @@
 let socket = io("http://localhost:8080");
-console.log("socket info", socket, io);
 
 function init() {
   //console.log("ran the init!", orbs);
@@ -10,9 +9,28 @@ function init() {
 socket.on("init-response", (data) => {
   orbs = data.orbs;
   // console.log("got data", data);
+  setInterval(() => {
+    socket.emit("tick", { xVector: player.xVector, yVector: player.yVector });
+  }, 33);
 });
 
 socket.on("tock", (data) => {
-  console.log("tock", data);
   allPlayerData = data.allPlayerData;
+});
+socket.on("player-pos", (data) => {
+  player.posX = data.clientPosX;
+  player.posY = data.clientPosY;
+});
+
+socket.on("orb-collision", (data) => {
+  orbs.splice(data.orbIndex, 1, data.newOrb);
+});
+socket.on("update-leaderboard", (data) => {
+  console.log("data!!", data);
+  document.querySelector(".leader-board").innerHTML = "";
+  data.forEach((currPlayer) => {
+    document.querySelector(
+      ".leader-board"
+    ).innerHTML += `<li class="leaderboard-player">${currPlayer.name} -${currPlayer.score} </li>`;
+  });
 });
