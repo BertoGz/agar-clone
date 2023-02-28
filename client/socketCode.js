10679517444702b64f46a84a4d4064d02b0e60b2
bertogz <bertogzlx@gmail.com>
2023-02-27 17:37:33 -0800

    add init func

--- client/socketCode.js
+++ client/socketCode.js
@@ -1,4 +1,18 @@
-let socket = io.connect("http://localhost:8080");
-socket.on("init", (data) => {
-  console.log("got data", data);
+let socket = io("http://localhost:8080");
+console.log("socket info", socket, io);
+
+function init() {
+  //console.log("ran the init!", orbs);
+  draw();
+  socket.emit("init", { playerName: player.name });
+}
+
+socket.on("init-response", (data) => {
+  orbs = data.orbs;
+  // console.log("got data", data);
+});
+
+socket.on("tock", (data) => {
+  console.log("tock", data);
+  allPlayerData = data.allPlayerData;
 });
