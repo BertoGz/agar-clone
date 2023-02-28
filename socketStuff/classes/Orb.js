7f93706941ee33223ce66ca4bdda75a1d109ad21
bertogz <bertogzlx@gmail.com>
2023-02-27 17:38:49 -0800

    add orb class

--- socketStuff/classes/Orb.js
+++ socketStuff/classes/Orb.js
@@ -1,15 +1,14 @@
-const { WORLD_SIZE } = require("../../Constants");
-
 function getRandomColor() {
   const randColor = () => Math.floor(Math.random() * 255);
   return `rgb(${randColor()},${randColor()},${randColor()})`;
 }
 class Orb {
-  constructor() {
+  constructor(settings) {
     this.color = getRandomColor();
-    this.posX = Math.floor(Math.random() * WORLD_SIZE);
-    this.posY = Math.floor(Math.random() * WORLD_SIZE);
+    this.posX = Math.floor(Math.random() * settings.worldSize);
+    this.posY = Math.floor(Math.random() * settings.worldSize);
     this.radius = 5;
   }
 }
 module.exports = Orb;
+    
