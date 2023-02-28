6d701683e8babfb1fbc06b878321d601565973a5
bertogz <bertogzlx@gmail.com>
2023-02-28 14:08:14 -0800

    continue work on tick func

--- socketStuff/socketMain.js
+++ socketStuff/socketMain.js
@@ -4,13 +4,16 @@ const Orb = require("./classes/Orb");
 const Player = require("./classes/Player");
 const PlayerConfig = require("./classes/PlayerConfig");
 const PlayerData = require("./classes/PlayerData");
-console.log("io", io);
+const {
+  checkForOrbCollisions,
+  checkForPlayerCollisions,
+} = require("./checkCollision");
 let orbs = [];
 let allPlayerData = [];
 let settings = {
-  defaultOrbsCount: 500,
-  defaultSpeed: 1,
-  defaultOrbSize: 6,
+  defaultOrbsCount: 50,
+  defaultSpeed: 5,
+  defaultOrbRadius: 6,
   defaultZoom: 1.5,
   worldSize: 500,
 };
@@ -21,24 +24,98 @@ function initOrbs() {
   }
 }
 initOrbs();
-// when io sees a socket connection we will broadcast the orbs
-
-//issue a message every 30 secs
-// there are 30 33s in 1000ms
 setInterval(() => {
-  io.to("game-name-space").emit("tock", { allPlayerData });
+  if (allPlayerData.length > 0) {
+    io.to("game-name-space").emit("tock", {
+      allPlayerData,
+    });
+  }
 }, 33);
-
 io.on("connect", (socket) => {
+  let player = {};
   socket.on("init", (payload) => {
     const { playerName } = payload || {};
     socket.join("game-name-space");
     console.log("a socket connected", socket.id);
     let playerConfig = new PlayerConfig(settings);
     let playerData = new PlayerData(playerName, settings);
-    let player = new Player(socket.io, playerConfig, playerData);
+    player = new Player(socket.io, playerConfig, playerData);
+
+    setInterval(() => {
+      socket.emit("player-pos", {
+        clientPosX: player.playerData.posX,
+        clientPosY: player.playerData.posY,
+      });
+    }, 33);
+
     socket.emit("init-response", { orbs });
     allPlayerData.push(playerData);
   });
+  socket.on("tick", (data) => {
+    const { xVector, yVector } = data || {};
+    if (!player?.playerConfig) {
+      return;
+    }
+    speed = player.playerConfig.speed;
+    xV = player.playerConfig.xVector = xVector;
+    yV = player.playerConfig.yVector = yVector;
+    // update player pos
+    if (
+      (player.playerData.posX < 5 && xV < 0) ||
+      (player.playerData.posX > settings.worldSize && xV > 0)
+    ) {
+      player.playerData.posY -= speed * yV;
+    } else if (
+      (player.playerData.posY < 5 && yV > 0) ||
+      (player.playerData.posY > settings.worldSize && yV < 0)
+    ) {
+      player.playerData.posX += speed * xV;
+    } else {
+      player.playerData.posX += speed * xV;
+      player.playerData.posY -= speed * yV;
+    }
+
+    let capturedOrb = checkForOrbCollisions(
+      player.playerData,
+      player.playerConfig,
+      orbs,
+      settings
+    );
+    capturedOrb
+      .then((d) => {
+        // run if collision happened
+        // returns captured orb index
+        const orbData = {
+          orbIndex: d,
+          newOrb: orbs[d],
+        };
+        // emit to clients about this
+        socket.emit("orb-collision", orbData);
+        socket.emit("update-leaderboard", getLeaderBoard());
+      })
+      .catch((d) => {});
+    let playerDeath = checkForPlayerCollisions(
+      player.playerData,
+      player.playerConfig,
+      allPlayerData,
+      player.socketId
+    );
+    playerDeath
+      .then((data) => {
+        console.log("a player collision!", data);
+        socket.emit("update-leaderboard", getLeaderBoard( ));
+      })
+      .catch(() => {});
+  });
 });
+function getLeaderBoard() {
+  // sort player array by score
+  allPlayerData.sort((a, b) => {
+    return b.score - a.score;
+  });
+  let leaderboard = allPlayerData.map((currPlayer) => {
+    return { name: currPlayer.playerName, score: currPlayer.score };
+  });
+  return leaderboard;
+}
 module.exports = io;
